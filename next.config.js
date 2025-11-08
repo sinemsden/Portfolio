fec2374a9aeb88fc15a8e2c0d667ea983401415f
SinemCevik <sinemcevik@SinemCeviks-MacBook-Air.local>
2025-11-08 22:39:17 +0300

    Simplify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
-  distDir: 'out',
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     unoptimized: true,
   },
   basePath: '/Portfolio',
   assetPrefix: '/Portfolio',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig;
